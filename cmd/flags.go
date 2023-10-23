7f08ef57a9aa272ff9a0053665c0e5353108a4e0
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-10-23 11:57:35 -0500

    add back example/Mocktimism.toml

--- cmd/flags.go
+++ cmd/flags.go
@@ -7,7 +7,7 @@ import (
 var (
 	ConfigFlag = &cli.StringFlag{
 		Name:    "config",
-		Value:   "../mocktimism.toml",
+		Value:   "./mocktimism.toml",
 		Aliases: []string{"c"},
 		Usage:   "path to config file",
 		EnvVars: []string{"MOCKTIMISM_CONFIG"},
