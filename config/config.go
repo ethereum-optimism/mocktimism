4b100b1530af72512c1efebe798c1d6977905f55
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-10-26 16:46:24 -0700

    make build test and lint pass

--- config/config.go
+++ config/config.go
@@ -18,40 +18,40 @@ type Profile struct {
 }
 
 type Chain struct {
-	// The mocktimism name of the chain
-	ID string `toml:"id"`
+	// The mocktimism name of the chain.
+	Name string `toml:"name"`
+	// Set the chain id
+	ChainID uint `toml:"chain_id"`
 	// The base chain ID when the chain is a rollup
 	// If set to 0 or the current chain's ID, the chain is considered an L1 chain
-	BaseChainID int64 `toml:"base_chain_id"`
+	BaseChainID uint `toml:"base_chain_id"`
 	// Specify chain id to skip fetching it from remote endpoint. This enables offline-start mode.
 	// You still must pass both `--fork-url` and `--fork-block-number`, and already have your required state cached on disk, anything missing locally would be fetched
 	// from the remote.
-	ForkChainID int64 `toml:"fork_chain_id"`
+	ForkChainID uint `toml:"fork_chain_id"`
 	// Fetch state over a remote endpoint instead of starting from an empty state.
 	// If you want to fetch state from a specific block number, add a block number like `http://localhost:8545@1400000` or use the `fork-block-number` option.
 	ForkURL string `toml:"fork_url"`
 	// The base fee in a block
-	BlockBaseFeePerGas int64 `toml:"block_base_fee_per_gas"`
-	// Set the chain id
-	ChainID int64 `toml:"chain_id"`
+	BlockBaseFeePerGas uint `toml:"block_base_fee_per_gas"`
 	// Set the gas limit
-	GasLimit int64 `toml:"gas_limit"`
+	GasLimit uint `toml:"gas_limit"`
 	// The number of accounts to pre-fund
-	Accounts int `toml:"accounts"`
+	Accounts uint `toml:"accounts"`
 	// The initial balance of each account
-	Balance int `toml:"balance"`
+	Balance uint `toml:"balance"`
 	// Enable steps tracing used for debug calls returning geth-style traces
 	StepsTracing bool `toml:"steps-tracing"`
 	//  Set the CORS allow_origin
 	AllowOrigin string `toml:"allow-origin"`
 	// The port the server will listen on
-	Port int `toml:"port"`
+	Port uint `toml:"port"`
 	// The host the server will listen on
 	Host string `toml:"host"`
 	// Block time in seconds for interval mining.
-	BlockTime int `toml:"block_time"`
+	BlockTime uint `toml:"block_time"`
 	//  Don't keep full chain history. If a number argument is specified, at most this number of states is kept in memory.
-	PruneHistory int `toml:"prune_history"`
+	PruneHistory uint `toml:"prune_history"`
 }
 
 var DefaultProfile = Profile{
@@ -59,7 +59,7 @@ var DefaultProfile = Profile{
 	Silent: true,
 	Chains: []Chain{
 		{
-			ID:                 "L1",
+			Name:               "L1",
 			BaseChainID:        900,
 			ForkChainID:        900,
 			ForkURL:            "",
@@ -76,7 +76,7 @@ var DefaultProfile = Profile{
 			PruneHistory:       0,
 		},
 		{
-			ID:                 "L2",
+			Name:               "L2",
 			BaseChainID:        901,
 			ForkChainID:        901,
 			ForkURL:            "",
