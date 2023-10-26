4a84048c01a5c3c6c1985e38d83e0f0080f040bc
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-10-26 16:17:19 -0700

    feat: Add anvil service to cli

--- cmd/cli_test.go
+++ cmd/cli_test.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/ethereum-optimism/mocktimism/config"
+	"github.com/ethereum-optimism/mocktimism/services/anvil"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/pelletier/go-toml"
 	"github.com/stretchr/testify/require"
 )
@@ -273,4 +277,88 @@ prune_history = 0
 
 	expectedBytes, _ := json.MarshalIndent(expectedConfig, "", "\t")
 	require.Equal(t, string(expectedBytes), string(out))
+}
+
+func TestCliAnvilCommand(t *testing.T) {
+	// Create a temp file to act as the config
+	tmpfile, err := os.CreateTemp("", "test.toml")
+	require.NoError(t, err)
+	defer os.Remove(tmpfile.Name())
+	defer tmpfile.Close()
+
+	// TODO can move this to shared test fixture folder https://github.com/ethereum-optimism/mocktimism/issues/29
+	testData := `
+[profile.default]
+
+[[profile.default.chains]]
+port = 8545
+host = "127.0.0.1"
+
+# l2 chain
+[[profile.default.chains]]
+port = 9545
+host = "127.0.0.1"
+`
+
+	data := []byte(testData)
+	err = os.WriteFile(tmpfile.Name(), data, 0644)
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	app := newCli("testCommit", "testDate")
+
+	go func() {
+		err := app.RunContext(ctx, []string{"appName", "anvil", "--config", tmpfile.Name()})
+		require.NoError(t, err)
+	}()
+
+	// Poll for l1 chain healtcheck
+	timeout := time.After(2 * time.Second)
+	ticker := time.NewTicker(200 * time.Millisecond) // polling every 200ms
+	defer ticker.Stop()
+
+	healthy := false
+loop:
+	for {
+		select {
+		case <-timeout:
+			break loop
+		case <-ticker.C:
+			// Check l1
+			service, err := anvil.NewAnvilService(
+				"HealthCheck",
+				log.New("module", "test"),
+				config.Chain{
+					Host: "127.0.0.1",
+					Port: 8545,
+				})
+			if err != nil {
+				break loop
+			}
+			healthy, err = service.HealthCheck()
+			if healthy || err != nil {
+				break loop
+			}
+			// Check l2
+			service, err = anvil.NewAnvilService(
+				"HealthCheck",
+				log.New("module", "test"),
+				config.Chain{
+					Host: "127.0.0.1",
+					Port: 9545,
+				})
+			if err != nil {
+				break loop
+			}
+			healthy, err = service.HealthCheck()
+			if healthy || err != nil {
+				break loop
+			}
+		}
+	}
+
+	require.NoError(t, err, "Health check failed")
+	require.True(t, healthy, "Service is not healthy after waiting for 2 seconds")
 }
