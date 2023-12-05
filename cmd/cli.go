86c089340a9df0e0a8fb73f39d6655474d2bebe8
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-12-04 18:48:55 -0800

    geth l2 working

--- cmd/cli.go
+++ cmd/cli.go
@@ -133,6 +133,74 @@ func newCli(GitCommit string, GitDate string) *cli.App {
 				},
 			},
 			{
+				Name:        "geth-l2",
+				Flags:       configFlags,
+				Description: "Starts the geth l2 services",
+				Action: func(ctx *cli.Context) error {
+					log := oplog.NewLogger(oplog.AppOut(ctx), oplog.ReadCLIConfig(ctx)).New("role", "mocktimism")
+					cfg, err := config.LoadNewConfig(log, ctx.String(ConfigFlag.Name))
+					gc := geth.GethConfig{
+						OpGeth:    true,
+						HTTPPort:  int(cfg.Profiles["default"].Chains[1].Port),
+						Verbosity: 3,
+						DataDir:   "/tmp/gethl2",
+					}
+					service, err := geth.NewGeth("l1", log, gc, nil)
+					if err != nil {
+						log.Error("failed to create geth service", "err", err)
+						return err
+					}
+					var wg sync.WaitGroup
+					errCh := make(chan error, 5)
+					processCtx, processCancel := context.WithCancel(ctx.Context)
+					runService := func(start func(ctx context.Context) error) {
+						wg.Add(1)
+						log.Info("starting new service...")
+						go func() {
+							log.Info("yes starting new service...")
+							defer func() {
+								log.Info("Ending")
+								if err := recover(); err != nil {
+									log.Error("Mocktimism had an unexpected fatal error", "err", err)
+									debug.PrintStack()
+									errCh <- fmt.Errorf("panic: %v", err)
+								}
+
+								processCancel()
+								wg.Done()
+							}()
+
+							log.Info("Starting service")
+							errCh <- start(processCtx)
+						}()
+					}
+
+					if true {
+						service.Start(processCtx)
+						ticker := time.NewTicker(2 * time.Second)
+						defer ticker.Stop()
+						for {
+							select {
+							case <-ticker.C:
+								healthy, err := service.HealthCheck()
+								if err != nil || !healthy {
+									log.Error("Health check failed:", err)
+									processCancel()
+									return nil
+								}
+								log.Info("health check passed")
+							case <-ctx.Done():
+								return nil
+							}
+						}
+					} else {
+						runService(service.Start)
+						log.Info("Ending service")
+						return nil
+					}
+				},
+			},
+			{
 				Name:        "geth-l1",
 				Flags:       configFlags,
 				Description: "Starts the geth l1 services",
