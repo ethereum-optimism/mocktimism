54d46ac757a228b11d6527e48dc3fdaed79bd68d
Will Cory <willcory@Wills-MacBook-Pro.local>
2024-02-21 15:40:33 -0800

    init every service

--- mocktimism.go
+++ mocktimism.go
@@ -2,6 +2,7 @@ package mocktimism
 
 import (
 	"context"
+	"errors"
 	"sync/atomic"
 
 	"github.com/ethereum-optimism/mocktimism/config"
@@ -25,7 +26,11 @@ func NewMocktimism(
 	shutdown context.CancelCauseFunc,
 	cfg *config.Config,
 ) (*Mocktimism, error) {
-	return &Mocktimism{log: log, shutdown: shutdown, cfg: cfg}, nil
+	out := &Mocktimism{log: log, shutdown: shutdown, cfg: cfg}
+	if err := out.initServices(ctx); err != nil {
+		return nil, errors.Join(err, out.Stop(ctx))
+	}
+	return out, nil
 }
 
 func (m *Mocktimism) Start(ctx context.Context) error {
@@ -42,4 +47,50 @@ func (m *Mocktimism) Stop(ctx context.Context) error {
 
 func (m *Mocktimism) Stopped() bool {
 	return m.stopped.Load()
+}
+
+// Inits every service in mocktimism
+// we don't worry about initing the challenger because we are not using it
+func (m *Mocktimism) initServices(ctx context.Context) error {
+	// init anvil L1
+	if err := m.initAnvilL1(ctx); err != nil {
+		return err
+	}
+	// init anvil L2
+	if err := m.initAnvilL2(ctx); err != nil {
+		return err
+	}
+	// init op node
+	if err := m.initOpNode(ctx); err != nil {
+		return err
+	}
+	// init op proposer
+	if err := m.initOpProposer(ctx); err != nil {
+		return err
+	}
+	// init op batcher
+	if err := m.initOpBatcher(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *Mocktimism) initAnvilL1(ctx context.Context) error {
+	return nil
+}
+
+func (m *Mocktimism) initAnvilL2(ctx context.Context) error {
+	return nil
+}
+
+func (m *Mocktimism) initOpNode(ctx context.Context) error {
+	return nil
+}
+
+func (m *Mocktimism) initOpProposer(ctx context.Context) error {
+	return nil
+}
+
+func (m *Mocktimism) initOpBatcher(ctx context.Context) error {
+	return nil
 }
